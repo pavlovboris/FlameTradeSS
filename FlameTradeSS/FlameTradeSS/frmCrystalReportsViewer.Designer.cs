dfdf483a60d25ed9911af6971a218db9bdf04b4f
Borislav Pavlov <pavlovb@yahoo.com>
2022-07-14 08:22:42 +0300

    43534

---
+++ FlameTradeSS/FlameTradeSS/frmCrystalReportsViewer.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace FlameTradeSS
+{
+    partial class frmCrystalReportsViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.rptLoadSingleDocument2 = new FlameTradeSS.rptLoadSingleDocument();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer
+            // 
+            this.crystalReportViewer.ActiveViewIndex = 0;
+            this.crystalReportViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer.Name = "crystalReportViewer";
+            this.crystalReportViewer.ReportSource = this.rptLoadSingleDocument2;
+            this.crystalReportViewer.Size = new System.Drawing.Size(800, 450);
+            this.crystalReportViewer.TabIndex = 0;
+            this.crystalReportViewer.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.ParameterPanel;
+            this.crystalReportViewer.Load += new System.EventHandler(this.crystalReportViewer_Load);
+            // 
+            // frmCrystalReportsViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.crystalReportViewer);
+            this.Name = "frmCrystalReportsViewer";
+            this.Text = "frmCrystalReportsViewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer;
+        private rptLoadSingleDocument rptLoadSingleDocument2;
+    }
+}
---
+++ FlameTradeSS/FlameTradeSS/frmCrystalReportsViewer.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlameTradeSS
+{
+    public partial class frmCrystalReportsViewer : Form
+    {
+        public frmCrystalReportsViewer()
+        {
+            InitializeComponent();
+        }
+
+        public FlameTradeDbEntities db;
+        public Documents document;
+
+        private void crystalReportViewer_Load(object sender, EventArgs e)
+        {
+           /* string connectionString = @"Data Source=definedsolutions-sql-server.database.windows.net;Initial Catalog=FlameTradeDb;User ID=CstmDBDefSol;Password=uncloak-TAIWAN-peccary-listless;"; 
+
+
+            SqlConnection mySqlConnection = new SqlConnection(connectionString);
+            SqlCommand mySqlCommand = new SqlCommand("select * from Documents;select * from DocumentSequences",mySqlConnection);
+            SqlDataAdapter mySqlDataAdapter = new SqlDataAdapter(mySqlCommand);
+
+            DataSet ds = new DataSet();
+
+            mySqlDataAdapter.Fill(ds);*/
+
+
+           // rptLoadSingleDocument2.SetDatabaseLogon("CstmDBDefSol","uncloak-TAIWAN-peccary-listless");
+            /*rptLoadSingleDocument1.Database.Tables["Documents"].SetDataSource(ds.Tables["Documents"]);
+            rptLoadSingleDocument1.Database.Tables["DocumentSequences"].SetDataSource(ds.Tables["DocumentSequences"]);*/
+
+        }
+    }
+}
